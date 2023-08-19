e82bfc0e3647787d1753f4e2646e0ebdb34fa760
Mthokozisi Zwane <mthokozisi.zwane1999@gmail.com>
2023-08-19 08:44:23 -0700

    modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    sums = 0
+    for i in range(len(sys.argv) - 1):
+        sums += int(sys.argv[i + 1])
+    print("{}".format(sums))
