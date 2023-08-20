4688458a0fe9b73698b94174fd0f9680d97a5268
Mthokozisi Zwane <mthokozisi.zwane1999@gmail.com>
2023-08-20 04:37:53 -0700

    data structures

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    
+    multi2 = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            multi2.append(True)
+        else:
+            multi2.append(False)
+
+    return (multi2)
