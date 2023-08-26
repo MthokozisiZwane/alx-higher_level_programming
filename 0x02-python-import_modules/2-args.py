c3d3d15e2d6cf863cf1f4ff9eec7ea2e437db2bb
Mthokozisi Zwane <mthokozisi.zwane1999@gmail.com>
2023-08-26 12:10:15 -0700

    importing modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
+#!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-
-    import sys
-
-    counts = len(sys.argv) - 1
-    if counts == 0:
+    argc = len(sys.argv) - 1
+    if argc == 0:
         print("0 arguments.")
-    elif counts == 1:
+    elif argc == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(counts))
-    for i in range(counts):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{} arguments:".format(argc))
+
+    for i in range(1, argc + 1):
+        print("{}: {}".format(i, sys.argv[i]))
