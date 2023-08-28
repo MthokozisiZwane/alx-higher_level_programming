6bb61d37682ef5bee7b460fdd5623139a35bd913
Mthokozisi Zwane <mthokozisi.zwane1999@gmail.com>
2023-08-28 14:27:47 -0700

    Python Exceptions

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=sys.stderr)
+        return None
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     numb_integers = 0
 
     try:
         for i in range(x):
-            if isinstance(my_list[i], int):
+            if i < len(my_list) and isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end=" ")
                 numb_integers += 1
     except (IndexError, TypeError):
