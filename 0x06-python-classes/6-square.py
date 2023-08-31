f85ca288f621e9ab1352ee795c7836fc7175b295
Mthokozisi Zwane <mthokozisi.zwane1999@gmail.com>
2023-08-31 13:36:39 -0700

    Python Classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -23,7 +23,7 @@ class Square:
         if not isinstance(position, tuple) or len(position) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if not all(isinstance(coordin, int) and
-                   coord >= 0 for coordin in position):
+                   coordin >= 0 for coordin in position):
             raise ValueError("position must be a tuple of 2 positive integers")
         self.__size = size
         self.__position = position
