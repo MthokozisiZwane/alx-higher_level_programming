83c2200feda826c62ebcd428bbb592a27898b381
Mthokozisi Zwane <mthokozisi.zwane1999@gmail.com>
2023-09-11 14:58:18 -0700

    Python inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" A Class that inherits from list"""
+""" A Class that inherits attributes of list"""
 
 
 class MyList(list):
-    """A custom list class that inherits the attributesof built-in list."""
+    """A custom list class that inherits the attributes of list."""
 
     def print_sorted(self):
         """Prints the list in ascending sorted order."""
