dcbaddabfc3384aad3b0507e2aa814908536bb2b
Mthokozisi Zwane <mthokozisi.zwane1999@gmail.com>
2023-09-18 17:03:58 -0700

    Rectaangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,83 +1,119 @@
 #!/usr/bin/python3
 """
-importing the Base class
+This module defines the Rectangle class, which inherits from the Base class.
 """
 
 from models.base import Base
 
 
-"""
-The rectangle class that inherits fron the Base class
-"""
-
-
 class Rectangle(Base):
     """
-    Initializing the variables
+    Represents a rectangle.
+
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+        x (int): The x-coordinate of the rectangle's position.
+        y (int): The y-coordinate of the rectangle's position.
+        id (int): The unique identifier for the rectangle.
     """
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes a new Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): The x-coordinate of the \
+                    rectangle's position (default is 0).
+            y (int, optional): The y-coordinate of the\
+                    rectangle's position (default is 0).
+            id (int, optional): The unique identifier \
+                    for the rectangle (default is None).
+        """
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
-        """ getter and setter methods for width
+    @property
+    def width(self):
         """
-        @property
-        def width(self):
-            return self.__width
+        Gets the width of the rectangle.
 
+        Returns:
+            int: The width of the rectangle.
         """
-        The setter method for the width
-        """
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            self.__with = value
+    @width.setter
+    def width(self, value):
+        """
+        Sets the width of the rectangle.
 
+        Args:
+            value (int): The new width value.
         """
-        Getter and setter method for height
-        """
+        self.__width = value
 
-        @property
-        def height(self):
-            return self.__height
+    @property
+    def height(self):
+        """
+        Gets the height of the rectangle.
 
+        Returns:
+            int: The height of the rectangle.
         """
-        The setter method for the height
-        """
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """
+        Sets the height of the rectangle.
 
+        Args:
+            value (int): The new height value.
         """
-        Getter and setter methods for x
-        """
-        @property
-        def x(self):
-            return self.__x
+        self.__height = value
 
+    @property
+    def x(self):
         """
-        The setter method for x
-        """
+        Gets the x-coordinate of the rectangle's position.
 
-        @x.setter
-        def x(self, value):
-            self.__x = value
+        Returns:
+            int: The x-coordinate of the rectangle's position.
+        """
+        return self.__x
 
+    @x.setter
+    def x(self, value):
         """
-        Getter and setter methods for y
-        """
-        @property
-        def y(self):
-            return self.__y
+        Sets the x-coordinate of the rectangle's position.
 
+        Args:
+            value (int): The new x-coordinate value.
         """
-        The setter method for y
-        """
+        self.__x = value
 
-        @y.setter
-        def y(self, value):
-            self.__y = value
+    @property
+    def y(self):
+        """
+        Gets the y-coordinate of the rectangle's position.
+
+        Returns:
+            int: The y-coordinate of the rectangle's position.
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        Sets the y-coordinate of the rectangle's position.
+
+        Args:
+            value (int): The new y-coordinate value.
+        """
+        self.__y = value
