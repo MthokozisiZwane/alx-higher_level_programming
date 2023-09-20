d9803a3a187a118057b971f71048c7a54541beb5
Mthokozisi Zwane <mthokozisi.zwane1999@gmail.com>
2023-09-20 08:00:26 -0700

    JSON string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,3 +31,25 @@ class Base:
         if not list_dictionaries or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Saves a list of objects to JSON file
+        This class method takes a list of objects and serializes\
+                them to a JSON file with the name of the class
+        appended with '.json'. It converts each object into a dictionary\
+            representation using the 'to_dictionary'
+        method of the objects and then writes the list o\
+            f dictionaries to the JSON file.
+        """
+
+        filename = cls.__name__ + ".json"
+        json_list = []
+
+        if list_objs is not None:
+            for obj in list_objs:
+                json_list.append(obj.to_dictionary())
+
+        with open(filename, "w") as file:
+            file.write(cls.to_json_string(json_list))
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,9 @@
 # tests/test_base.py
 import unittest
 import json
+from models.rectangle import Rectangle
+#from models.base import Base
+
 
 #from base import Base #this is for my working on spyder
 from models.base import Base  #this is the line you should have in vim
@@ -34,6 +37,31 @@ class TestBaseToJsonString(unittest.TestCase):
         # Test when None is provided
         result = Base.to_json_string(None)
         self.assertEqual(result, "[]")
+
+#test save to file
+class TestSaveToFile(unittest.TestCase):
+
+    def test_save_to_file(self):
+        # Creating instances of classes
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+
+        # Saving the instances to a JSON file
+        Rectangle.save_to_file([r1, r2])
+
+        # Read the contents of the saved JSON file
+        with open("Rectangle.json", "r") as file:
+            json_content = file.read()
+
+        # the expected JSON representation of the objects
+        expected_json = '[{"x": 2, "y": 8, "id": 1, "width": 10, "height": 7},' \
+                        '{"x": 0, "y": 0, "id": 2, "width": 2, "height": 4}]'
+
+        # Convert JSON strings to sets of frozensets for comparison
+        actual_set = {frozenset(item.items()) for item in json.loads(json_content)}
+        expected_set = {frozenset(item.items()) for item in json.loads(json_content)}
+
+        self.assertEqual(actual_set, expected_set)
 
 if __name__ == '__main__':
     unittest.main()
