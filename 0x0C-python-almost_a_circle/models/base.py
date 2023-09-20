417743171e5d8504c560cbb1f0c39b36a89ee118
Mthokozisi Zwane <mthokozisi.zwane1999@gmail.com>
2023-09-20 06:10:28 -0700

    Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@
 """
 The base class for all classes in this project
 """
+import json
 
 
 class Base:
@@ -22,3 +23,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Returns the json string representation of list_dictionaries
+        """
+        if not list_dictionaries or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,5 +1,7 @@
 # tests/test_base.py
 import unittest
+import json
+
 #from base import Base #this is for my working on spyder
 from models.base import Base  #this is the line you should have in vim
 
@@ -14,6 +16,24 @@ class TestBase(unittest.TestCase):
         b2 = Base()
         self.assertEqual(b1.id, 1)
         self.assertEqual(b2.id, 2)
+
+class TestBaseToJsonString(unittest.TestCase):
+    def test_empty_list(self):
+        # Test when an empty list is provided
+        result = Base.to_json_string([])
+        self.assertEqual(result, "[]")
+
+    def test_list_with_dicts(self):
+        # Test when a list of dictionaries is provided
+        dictionaries = [{'id': 1, 'name': 'Bhoke'}, {'id': 2, 'name': 'Mtho'}]
+        result = Base.to_json_string(dictionaries)
+        expected_result = '[{"id": 1, "name": "Bhoke"}, {"id": 2, "name": "Mtho"}]'
+        self.assertEqual(result, expected_result)
+
+    def test_with_none(self):
+        # Test when None is provided
+        result = Base.to_json_string(None)
+        self.assertEqual(result, "[]")
 
 if __name__ == '__main__':
     unittest.main()
