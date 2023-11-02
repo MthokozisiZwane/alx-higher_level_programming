5ed75d89fdda1d4d83478ac132624e6c735c857c
Mthokozisi Zwane <mthokozisi.zwane1999@gmail.com>
2023-11-02 03:46:02 -0700

    sql more queries

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,8 @@
--- 103-rating_genres.sql
-SELECT tv_genres.name, SUM(rating) AS rating_sum
+-- lists all genres in the database hbtn_0d_tvshows_rate by their rating
+-- lists all rows in a database linked to a row in another table
+SELECT name, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-LEFT JOIN show_ratings ON tv_shows.id = show_ratings.show_id
-GROUP BY tv_genres.name
-ORDER BY rating_sum DESC;
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY name
+ORDER BY rating DESC;
