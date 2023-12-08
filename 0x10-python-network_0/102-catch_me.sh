eaa31543f02b6551e52a011c4e1bdec18d027e06
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2023-12-08 14:09:05 -0800

    updated

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script makes a request that causes the server to respond with "You got me!"
-curl -sL -X PUT -d "user_id=98" "$1/catch_me" -H "Origin: HolbertonSchool"
+curl -sLX PUT "$1/catch_me" -d "user_id=98" -H "Origin: HolbertonSchool" -H "Referer: 0.0.0.0:5000"
