63bb9e3c0e11b121fd41e7a5f17c9579822ece5c
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2023-12-08 12:58:00 -0800

    methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Takes a URL as an argument and displays all HTTP methods the server will accept
-url=$1
-curl -sI -X OPTIONS "$url" | grep -i Allow | awk '{print $2}'
+# This script takes a URL and displays all HTTP methods the server will accept
+curl -sI "$1" | grep -i Allow | cut -d' ' -f2-
