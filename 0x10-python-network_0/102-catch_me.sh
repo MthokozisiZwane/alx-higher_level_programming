1ef6ed09ad1e77e8fab7793a74fb6d75abccbe17
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2023-12-10 05:15:45 -0800

    catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script makes a request that causes the server to respond with "You got me!"
-curl -sLX PUT -H "Origin: HolbertonSchool" -H "Referer: 0.0.0.0:5000/catch_me" "$1/catch_me" -d "user_id=98" > /dev/null 2>&1
+curl -sLX PUT -H "Origin: HolbertonSchool" -H "Referer: 0.0.0.0:5000/catch_me" 0.0.0.0:5000/catch_me -d "user_id=98" -w "You find me!" -o /dev/null
